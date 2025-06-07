87ab72c34582b2deb80d03b8b9b7e759e15c0c57
Angel Cancho <u201721995@upc.edu.pe>
2025-06-07 16:56:37 -0500

    chore(app): add FormsModule and MatIconModule to app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ComponentSidebarComponent } from './shared/components/component-sidebar/component-sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     RouterOutlet,
     ComponentSidebarComponent,
     MatSidenavModule,
+    FormsModule,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
