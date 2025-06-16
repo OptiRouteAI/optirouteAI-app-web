25840a8b2dbeb09c846e027370cd9ad2583e1aaf
Angel Cancho <u201721995@upc.edu.pe>
2025-06-16 17:10:53 -0500

    refactor: update OrderService to use UrlConfigService for API URLs

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -2,25 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order, OrderDetail } from '../models/order.model';
+import { UrlConfigService } from '../../services/url-config-service.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  private readonly API_URL = 'http://127.0.0.1:8000/purchase/purchase-order/';
+  constructor(private http: HttpClient, private urlConfig: UrlConfigService) {}
 
-  constructor(private http: HttpClient) {}
-
-  /**
-   * GET /purchase/purchase-order/
-   * Obtiene la lista de pedidos desde el backend
-   */
   getOrders(): Observable<Order[]> {
-    return this.http.get<Order[]>(this.API_URL);
+    return this.http.get<Order[]>(this.urlConfig.ORDER_URL);
   }
 
   getOrderDetails(nroPedido: string): Observable<OrderDetail[]> {
-    const url = `${this.API_URL}${nroPedido}/details`; // URL con el nroPedido
+    const url = this.urlConfig.ORDER_DETAIL_URL.replace(
+      '{nroPedido}',
+      nroPedido
+    );
     return this.http.get<OrderDetail[]>(url);
   }
 }
