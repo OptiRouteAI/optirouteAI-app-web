79e74d016631b532fbf9f1f88840846c8e598b0f
Angel Cancho <u201721995@upc.edu.pe>
2025-06-17 23:41:03 -0500

    feat: add methods to OrderService for fetching orders and their details

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -10,10 +10,16 @@ import { UrlConfigService } from '../../services/url-config-service.service';
 export class OrderService {
   constructor(private http: HttpClient, private urlConfig: UrlConfigService) {}
 
+  /**
+   * Obtener todos los pedidos
+   */
   getOrders(): Observable<Order[]> {
     return this.http.get<Order[]>(this.urlConfig.ORDER_URL);
   }
 
+  /**
+   * Obtener detalles de un pedido por su número
+   */
   getOrderDetails(nroPedido: string): Observable<OrderDetail[]> {
     const url = this.urlConfig.ORDER_DETAIL_URL.replace(
       '{nroPedido}',
@@ -21,4 +27,12 @@ export class OrderService {
     );
     return this.http.get<OrderDetail[]>(url);
   }
+
+  /**
+   * Obtener el estado de los pedidos
+   */
+  getOrdersWithState(): Observable<any[]> {
+    return this.http.get<any[]>(this.urlConfig.ORDER_STATUS_URL);
+  }
+
 }
